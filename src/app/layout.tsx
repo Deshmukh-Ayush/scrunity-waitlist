d3ef4c079226b795a57a3ab99de86ac6cf837294
Ayush <ayushdeshmukh301@gmail.com>
2026-02-25 05:33:16 +0530

    feat: flushed the comps.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,12 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Instrument_Serif } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const instrumentSerif = Instrument_Serif({
+const insrif = Instrument_Serif({
   weight: "400",
   variable: "--font-instrument-serif",
   subsets: ["latin"],
 });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -31,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${instrumentSerif.variable} antialiased`}
+        className={`${insrif.className} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import Canvas from "@/components/canvas";
 
 export default function Home() {
   return (
     <div className="min-h-screen w-full bg-neutral-900">
-      <Canvas />
+      Hi
     </div>
   );
 }
